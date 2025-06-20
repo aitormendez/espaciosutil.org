c9d61d5a7533e222b0f1cba190856a57b4f3b9ea
Aitor Méndez <aitor@e451.net>
2025-06-20 10:09:35 +0200

    fix(video): corrige selector del bloque en los estilos del editor

--- site/web/app/plugins/espacio-sutil-blocks/build/video/index.asset.php
+++ site/web/app/plugins/espacio-sutil-blocks/build/video/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c599af1dc1e516e7b8d8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd5b237da2616e2d3c0f5');
