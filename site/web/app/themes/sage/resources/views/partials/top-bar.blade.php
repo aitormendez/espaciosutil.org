6ae5ec5d6f8716d41a1470ff348d00f21ac39b59
Aitor Méndez <aitor@e451.net>
2025-06-21 19:50:05 +0200

    feat(layout): corrección posicionamiento menú top bar

--- site/web/app/themes/sage/resources/views/partials/top-bar.blade.php
+++ site/web/app/themes/sage/resources/views/partials/top-bar.blade.php
@@ -1,8 +1,10 @@
 <div class="border-gris3 relative border-b py-1 font-sans font-light text-white xl:pr-12">
-    <div class="container mx-auto flex justify-center space-x-6 xl:justify-end">
+    <div class="mx-auto flex justify-center space-x-6 xl:!justify-end">
         <a href="{{ get_permalink(get_page_by_path('curso-online')) }}" class="transition hover:text-gray-300">Curso
             online</a>
-        <a href="{{ wc_get_page_permalink('shop') }}" class="transition hover:text-gray-300">Tienda</a>
+        @if (function_exists('wc_get_page_permalink'))
+            <a href="{{ wc_get_page_permalink('shop') }}" class="transition hover:text-gray-300">Tienda</a>
+        @endif
         <a href="{{ wp_login_url() }}" class="transition hover:text-gray-300">Acceso</a>
     </div>
 </div>
